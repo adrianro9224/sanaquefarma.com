26911363938f5957b91890aada3f8fffeaa249ae
adrianchief <adrian.romero9224@gmail.com>
2015-11-22 18:11:18 -0500

    add new buttons styles

--- application/views/templates/carousel.php
+++ application/views/templates/carousel.php
@@ -4,8 +4,6 @@
 	      	<slide ng-repeat="slide in slides" active="slide.active">
 	        	<img ng-src="{{slide.image}}" style="margin:auto;">
                 <a href="{{slide.button}}" class="{{slide.class}}">Agregar</a>
-	        	<div class="carousel-caption">
-	        	</div>
 	      	</slide>
     	</carousel>
   	</div>
