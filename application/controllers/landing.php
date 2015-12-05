97b302b5db420ebc9dc3c1985cf73662cb77c2ba
adrianchief <adrian.romero9224@gmail.com>
2015-12-05 16:25:12 -0500

    add landing options

---
+++ application/controllers/landing.php
@@ -0,0 +1,29 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Landing extends MY_controller {
+	/**
+     * Call to CI_controller constructor
+     */
+    function __construct() {
+        parent::__construct();
+    }
+
+    public function index( $landing_page = NULL ) {
+
+    	$landing_pages = array('bioplus');
+
+    	if( isset($landing_page) && in_array($landing_page, $landing_pages) ) {
+    		$data = array();
+    		$data['landing_resources_url'] = '/assets/landing_resources/'.$landing_page.'/';
+
+    		$this->load->view('landing_pages/'.$landing_page.'/index', $data);
+    	}else{
+    		show_404();
+    	}
+
+    }
+}
+
+
+?>
---
+++ application/views/landing_pages/bioplus/index.php
@@ -0,0 +1,53 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <meta name="generator" content="Serif WebPlus X7 (15,0,4,38)">
+    <meta name="viewport" content="width=960">
+    <link rel="stylesheet" type="text/css" href="<?= $landing_resources_url.'wpscripts/wpstyles.css'?>">
+    <style type="text/css">
+      .P-1 { text-align:justify;line-height:1px;font-family:"Arial", sans-serif;font-style:normal;font-weight:normal;color:#000000;background-color:transparent;font-variant:normal;font-size:17.0px;vertical-align:0; }
+      .C-1 { line-height:20.00px;font-family:"Arial", sans-serif;font-style:normal;font-weight:normal;color:#000000;background-color:transparent;text-decoration:none;font-variant:normal;font-size:17.3px;vertical-align:0; }
+      .P-2 { text-align:justify;text-indent:-24.0px;line-height:1px;font-family:"Arial", sans-serif;font-style:normal;font-weight:700;color:#000000;background-color:transparent;font-variant:normal;font-size:17.0px;vertical-align:0; }
+      .C-2 { line-height:21.58px;font-family:"Arial", sans-serif;font-style:normal;font-weight:700;color:#000000;background-color:transparent;text-decoration:none;font-variant:normal;font-size:17.3px;vertical-align:0; }
+      .C-3 { line-height:21.58px;font-family:"Arial", sans-serif;font-style:normal;font-weight:normal;color:#000000;background-color:transparent;text-decoration:none;font-variant:normal;font-size:17.3px;vertical-align:0; }
+      .OBJ-1,.OBJ-1:link,.OBJ-1:visited { background-image:url(<?= $landing_resources_url.'wpimages/wpc23d8769_06.png'?>);background-repeat:no-repeat;background-position:0px 0px;text-decoration:none;display:block;position:absolute; }
+      .OBJ-1:hover { background-position:0px -112px; }
+      .OBJ-1:active,a:link.OBJ-1.Activated,a:link.OBJ-1.Down,a:visited.OBJ-1.Activated,a:visited.OBJ-1.Down,.OBJ-1.Activated,.OBJ-1.Down { background-position:0px -56px; }
+      .OBJ-1:focus { outline-style:none; }
+      button.OBJ-1 { background-color:transparent;border:none 0px;padding:0;display:inline-block;cursor:pointer; }
+      button.OBJ-1:disabled { pointer-events:none; }
+      .OBJ-1.Inline { display:inline-block;position:relative;line-height:normal; }
+      .OBJ-1 span,.OBJ-1:link span,.OBJ-1:visited span { color:#ffffff;font-family:Arial,sans-serif;font-weight:bold;text-decoration:none;text-align:center;text-transform:none;font-style:normal;left:6px;top:16px;width:175px;height:25px;font-size:19px;display:block;position:absolute;cursor:pointer; }
+      .OBJ-1.Disabled span,a:link.OBJ-1.Disabled span,a:visited.OBJ-1.Disabled span,a:hover.OBJ-1.Disabled span,a:active.OBJ-1.Disabled span { top:15px; }
+    </style>
+    <script type="text/javascript" src="<?= $landing_resources_url.'wpscripts/jquery.js'?>"></script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+      $("a.ActiveButton").bind({ mousedown:function(){if ( $(this).attr('disabled') === undefined ) $(this).addClass('Activated');}, mouseleave:function(){ if ( $(this).attr('disabled') === undefined ) $(this).removeClass('Activated');}, mouseup:function(){ if ( $(this).attr('disabled') === undefined ) $(this).removeClass('Activated');}});
+      });
+    </script>
+  </head>
+  <body style="height:1000px;background:#ffffff;">
+    <div id="divMain" style="background:transparent;margin-left:auto;margin-right:auto;position:relative;width:960px;height:1000px;">
+      <img alt="" src="<?= $landing_resources_url.'wpimages/wp22e36e34_06.png'?>" style="position:absolute;left:214px;top:199px;width:277px;height:239px;">
+      <img alt="" src="<?= $landing_resources_url.'wpimages/wpc855fb11_06.png'?>" style="position:absolute;left:472px;top:174px;width:273px;height:290px;">
+      <img alt="SIMBIÓTICOS=" src="<?= $landing_resources_url.'wpimages/wp5c33c105_06.png'?>" style="position:absolute;left:242px;top:36px;width:482px;height:66px;">
+      <img alt="Probióticos + Prebióticos" src="<?= $landing_resources_url.'wpimages/wp8eb61a31_06.png'?>" style="position:absolute;left:231px;top:101px;width:497px;height:51px;">
+      <div style="position:absolute;left:24px;top:485px;width:911px;height:421px;overflow:hidden;">
+        <p class="Normal P-1"><span class="C-1">Los probióticos son microorganismos vivos que contribuyen a mantener el equilibrio microbiano del Tracto Gastro Intestinal (TGI). Beneficiando la salud intestinal de quien los consume.</span></p>
+        <p class="Normal P-1"><span class="C-1"><br></span></p>
+        <p class="Normal P-1"><span class="C-1">La ingesta regular de los Probióticos puede mejorar las diferentes enfermedades del colon entre otros:</span></p>
+        <p class="List-Paragraph P-2"><span class="C-2">1. Estreñimiento<span class="C-3">: gracias a los probióticos puedes mejorar y normalizar el tránsito intestinal aumentando el número de deposiciones. El efecto de los probióticos sobre el estreñimiento podría contribuir en una mejora de la flora intestinal en quienes padecen estreñimiento y promover así una mejoría de los movimientos intestinales. Los probióticos mejoran también la consistencia de las heces. Es importante acompañar a los probióticos con una dieta rica en fibra dietaría y abundante agua que dará al bolo fecal, volumen, peso y viscosidad lo cual acelerará el tránsito intestinal y favorecerá las evacuaciones.</span></span></p>
+        <p class="List-Paragraph P-2"><span class="C-2">2. Diarrea<span class="C-3">: En múltiples estudios los probióticos han demostrado ser eficaces en el tratamiento de la diarrea crónica y aguda en adultos y niños. Además de prevenir la diarrea asociada a antibióticos.</span></span></p>
+        <p class="List-Paragraph P-2"><span class="C-2">3. Colon Irritable<span class="C-3">: Los probióticos también pueden reducir la gravedad de la inflamación intestinal inducida por microbios, gastroenteritis aguda, la enfermedad del colon irritable y reducir el riesgo de cáncer colorrectal.</span></span></p>
+        <p class="List-Paragraph P-2"><span class="C-2">4. Intolerancia a la Lactosa<span class="C-3">: se ha observado que los probióticos mejoran la intolerancia a la lactosa.</span></span></p>
+        <p class="Body P-1"><span class="C-1">En general los estudios apuntan que la ingesta regular de probióticos y prebióticos mejoran la salud intestinal y la función inmunitaria.</span></p>
+      </div>
+      <a href="http://sanaquefarma.com/product/search_product/probiotico" target="_blank" id="btn_1" class="OBJ-1 ActiveButton" style="position:absolute;left:386px;top:927px;width:188px;height:56px;">
+        <span>¡&nbsp;Compre&nbsp;Ahora&nbsp;!</span>
+      </a>
+    </div>
+  </body>
+</html>
