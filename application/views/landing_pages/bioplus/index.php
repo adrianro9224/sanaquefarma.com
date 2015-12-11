8d4feef8b14affcde1ac02b21301eb9eeff8b9a6
adrianchief <adrian.romero9224@gmail.com>
2015-12-11 00:06:30 -0500

    new bioplus landing page

--- application/views/landing_pages/bioplus/index.php
+++ application/views/landing_pages/bioplus/index.php
@@ -9,10 +9,8 @@
     <style type="text/css">
       .P-1 { text-align:justify;line-height:1px;font-family:"Arial", sans-serif;font-style:normal;font-weight:normal;color:#000000;background-color:transparent;font-variant:normal;font-size:17.0px;vertical-align:0; }
       .C-1 { line-height:20.00px;font-family:"Arial", sans-serif;font-style:normal;font-weight:normal;color:#000000;background-color:transparent;text-decoration:none;font-variant:normal;font-size:17.3px;vertical-align:0; }
-      .P-2 { text-align:justify;text-indent:-24.0px;line-height:1px;font-family:"Arial", sans-serif;font-style:normal;font-weight:700;color:#000000;background-color:transparent;font-variant:normal;font-size:17.0px;vertical-align:0; }
-      .C-2 { line-height:21.58px;font-family:"Arial", sans-serif;font-style:normal;font-weight:700;color:#000000;background-color:transparent;text-decoration:none;font-variant:normal;font-size:17.3px;vertical-align:0; }
-      .C-3 { line-height:21.58px;font-family:"Arial", sans-serif;font-style:normal;font-weight:normal;color:#000000;background-color:transparent;text-decoration:none;font-variant:normal;font-size:17.3px;vertical-align:0; }
-      .OBJ-1,.OBJ-1:link,.OBJ-1:visited { background-image:url(<?= $landing_resources_url.'wpimages/wpc23d8769_06.png'?>);background-repeat:no-repeat;background-position:0px 0px;text-decoration:none;display:block;position:absolute; }
+      .P-2 { text-align:justify;margin-left:48.0px;text-indent:-15.0px;line-height:1px;font-family:"Arial", sans-serif;font-style:normal;font-weight:normal;color:#000000;background-color:transparent;font-variant:normal;font-size:17.0px;vertical-align:0; }
+      .OBJ-1,.OBJ-1:link,.OBJ-1:visited { background-image:url(<?= $landing_resources_url.'wpimages/wpf9691a6a_06.png'?>);background-repeat:no-repeat;background-position:0px 0px;text-decoration:none;display:block;position:absolute; }
       .OBJ-1:hover { background-position:0px -112px; }
       .OBJ-1:active,a:link.OBJ-1.Activated,a:link.OBJ-1.Down,a:visited.OBJ-1.Activated,a:visited.OBJ-1.Down,.OBJ-1.Activated,.OBJ-1.Down { background-position:0px -56px; }
       .OBJ-1:focus { outline-style:none; }
@@ -29,25 +27,21 @@
       });
     </script>
   </head>
-  <body style="height:1000px;background:#ffffff;">
+  <body style="height:1000px;background:#ffffff url(<?= $landing_resources_url.'wpimages/wp6ccf59b5_06.png'?>) no-repeat fixed center center;">
     <div id="divMain" style="background:transparent;margin-left:auto;margin-right:auto;position:relative;width:960px;height:1000px;">
-      <img alt="" src="<?= $landing_resources_url.'wpimages/wp22e36e34_06.png'?>" style="position:absolute;left:214px;top:199px;width:277px;height:239px;">
-      <img alt="" src="<?= $landing_resources_url.'wpimages/wpc855fb11_06.png'?>" style="position:absolute;left:472px;top:174px;width:273px;height:290px;">
-      <img alt="SIMBIÓTICOS=" src="<?= $landing_resources_url.'wpimages/wp5c33c105_06.png'?>" style="position:absolute;left:242px;top:36px;width:482px;height:66px;">
-      <img alt="Probióticos + Prebióticos" src="<?= $landing_resources_url.'wpimages/wp8eb61a31_06.png'?>" style="position:absolute;left:231px;top:101px;width:497px;height:51px;">
-      <div style="position:absolute;left:24px;top:485px;width:911px;height:421px;overflow:hidden;">
-        <p class="Normal P-1"><span class="C-1">Los probióticos son microorganismos vivos que contribuyen a mantener el equilibrio microbiano del Tracto Gastro Intestinal (TGI). Beneficiando la salud intestinal de quien los consume.</span></p>
+      <img alt="" src="<?= $landing_resources_url.'wpimages/wpf3975909_06.png'?>" style="position:absolute;left:236px;top:175px;width:255px;height:220px;">
+      <img alt="" src="<?= $landing_resources_url.'wpimages/wpd1d07106_06.png'?>" style="position:absolute;left:473px;top:152px;width:251px;height:267px;">
+      <img alt="Simbióticos = Probióticos + Prebióticos" src="<?= $landing_resources_url.'wpimages/wp4b4de53b_06.png'?>" style="position:absolute;left:213px;top:425px;width:542px;height:36px;">
+      <div style="position:absolute;left:24px;top:468px;width:911px;height:451px;overflow:hidden;">
+        <p class="Normal P-1"><span class="C-1">BioPlus+ es un potente Bioestimulador que ofrece una acción Simbiótica (Probióticos + Prebióticos). Incluye tres mezclas reconocidas y registradas de lactobacillus.</span></p>
         <p class="Normal P-1"><span class="C-1"><br></span></p>
-        <p class="Normal P-1"><span class="C-1">La ingesta regular de los Probióticos puede mejorar las diferentes enfermedades del colon entre otros:</span></p>
-        <p class="List-Paragraph P-2"><span class="C-2">1. Estreñimiento<span class="C-3">: gracias a los probióticos puedes mejorar y normalizar el tránsito intestinal aumentando el número de deposiciones. El efecto de los probióticos sobre el estreñimiento podría contribuir en una mejora de la flora intestinal en quienes padecen estreñimiento y promover así una mejoría de los movimientos intestinales. Los probióticos mejoran también la consistencia de las heces. Es importante acompañar a los probióticos con una dieta rica en fibra dietaría y abundante agua que dará al bolo fecal, volumen, peso y viscosidad lo cual acelerará el tránsito intestinal y favorecerá las evacuaciones.</span></span></p>
-        <p class="List-Paragraph P-2"><span class="C-2">2. Diarrea<span class="C-3">: En múltiples estudios los probióticos han demostrado ser eficaces en el tratamiento de la diarrea crónica y aguda en adultos y niños. Además de prevenir la diarrea asociada a antibióticos.</span></span></p>
-        <p class="List-Paragraph P-2"><span class="C-2">3. Colon Irritable<span class="C-3">: Los probióticos también pueden reducir la gravedad de la inflamación intestinal inducida por microbios, gastroenteritis aguda, la enfermedad del colon irritable y reducir el riesgo de cáncer colorrectal.</span></span></p>
-        <p class="List-Paragraph P-2"><span class="C-2">4. Intolerancia a la Lactosa<span class="C-3">: se ha observado que los probióticos mejoran la intolerancia a la lactosa.</span></span></p>
-        <p class="Body P-1"><span class="C-1">En general los estudios apuntan que la ingesta regular de probióticos y prebióticos mejoran la salud intestinal y la función inmunitaria.</span></p>
+        <p class="Normal P-1"><span class="C-1">La ingesta regular de BioPlus+ tiene múltiples beneficios como:</span></p>
+        <p class="Normal P-1"><span class="C-1"><br></span></p>
+        <ul style="list-style-type:disc;margin:0;padding:0;">
+          <li class="Body P-2" style="text-indent:0;margin-left:48.0px;"><span class="C-1">Restaura y estimula el crecimiento de la microflora intestinal.</span></li><li class="Body P-2" style="text-indent:0;margin-left:48.0px;"><span class="C-1">Apoya y fortalece el sistema inmunológico.</span></li><li class="Body P-2" style="text-indent:0;margin-left:48.0px;"><span class="C-1">Potente acción en diarrea asociada a la toma de antibióticos.</span></li><li class="Body P-2" style="text-indent:0;margin-left:48.0px;"><span class="C-1">Promueve la correcta digestión y absorción de nutrientes alimenticios.</span></li><li class="Body P-2" style="text-indent:0;margin-left:48.0px;"><span class="C-1">Facilita la erradicación de Helicobacter Pylori.</span></li><li class="Body P-2" style="text-indent:0;margin-left:48.0px;"><span class="C-1">Previene el síndrome de colon irritable.</span></li><li class="Body P-2" style="text-indent:0;margin-left:48.0px;"><span class="C-1">Previene los efectos por la intolerancia a la lactosa.</span></li><li class="Body P-2" style="text-indent:0;margin-left:48.0px;"><span class="C-1">Ayuda en estreñimiento, ritmo intestinal y salud colónica.</span></li><li class="Body P-2" style="text-indent:0;margin-left:48.0px;"><span class="C-1">Potencializa la absorción del calcio reduciendo la aparición de osteoporosis.</span></li><li class="Body P-2" style="text-indent:0;margin-left:48.0px;"><span class="C-1">Coadyuvante en el tratamiento de la vaginosis bacteriana y vaginitis por candida.</span></li><li class="Body P-2" style="text-indent:0;margin-left:48.0px;"><span class="C-1">Mejora la salud urinaria y vaginal.</span></li>
+        </ul>
       </div>
-      <a href="http://sanaquefarma.com/product/search_product/probiotico" target="_blank" id="btn_1" class="OBJ-1 ActiveButton" style="position:absolute;left:386px;top:927px;width:188px;height:56px;">
-        <span>¡&nbsp;Compre&nbsp;Ahora&nbsp;!</span>
-      </a>
+      <a href="http://sanaquefarma.com/product/search_product/bioplus" target="_blank" id="btn_1" class="OBJ-1 ActiveButton" style="position:absolute;left:386px;top:927px;width:188px;height:56px;"><span>¡&nbsp;Compre&nbsp;Ahora&nbsp;!</span></a><img alt="" src="<?= $landing_resources_url.'wpimages/wp90201623_06.png'?>" style="position:absolute;left:190px;top:13px;width:581px;height:132px;">
     </div>
   </body>
 </html>
