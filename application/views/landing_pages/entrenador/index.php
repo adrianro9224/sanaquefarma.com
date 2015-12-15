25d8e27f3c6a9bae9d63b60fdddc1c6182830c12
adrianchief <adrian.romero9224@gmail.com>
2015-12-15 16:55:46 -0500

    addd landing for entrenadorx

---
+++ application/views/landing_pages/entrenador/index.php
@@ -0,0 +1,52 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <meta name="generator" content="Serif WebPlus X7 (15,0,4,38)">
+    <meta name="viewport" content="width=960">
+    <meta name="keywords" content="Incontinencia Urinaria, embarazo, parto, piso pélvico, músculos vaginales, disfunción sexual, ejercicios de kegel, rehabilitación.">
+    <meta name="description" content="El Entrenador Vaginal está diseñado para dirigir a las mujeres en la práctica de los ejercicios de Kegel, con el fin de rehabilitar músculos pélvicos débiles.">
+    <link rel="stylesheet" type="text/css" href="<?= $landing_resources_url.'wpscripts/wpstyles.css'?>">
+    <style type="text/css">
+      .P-1 { text-align:justify;line-height:1px;font-family:"Arial", sans-serif;font-style:normal;font-weight:normal;color:#000000;background-color:transparent;font-variant:normal;font-size:17.0px;vertical-align:0; }
+      .C-1 { line-height:20.00px;font-family:"Arial", sans-serif;font-style:normal;font-weight:normal;color:#000000;background-color:transparent;text-decoration:none;font-variant:normal;font-size:17.3px;vertical-align:0; }
+      .C-2 { line-height:20.00px;font-family:"Arial", sans-serif;font-style:normal;font-weight:700;color:#000000;background-color:transparent;text-decoration:none;font-variant:normal;font-size:17.3px;vertical-align:0; }
+      .OBJ-1,.OBJ-1:link,.OBJ-1:visited { background-image:url(<?= $landing_resources_url.'wpimages/wp5b9148a4_06.png' ?>);background-repeat:no-repeat;background-position:0px 0px;text-decoration:none;display:block;position:absolute; }
+      .OBJ-1:hover { background-position:0px -112px; }
+      .OBJ-1:active,a:link.OBJ-1.Activated,a:link.OBJ-1.Down,a:visited.OBJ-1.Activated,a:visited.OBJ-1.Down,.OBJ-1.Activated,.OBJ-1.Down { background-position:0px -56px; }
+      .OBJ-1:focus { outline-style:none; }
+      button.OBJ-1 { background-color:transparent;border:none 0px;padding:0;display:inline-block;cursor:pointer; }
+      button.OBJ-1:disabled { pointer-events:none; }
+      .OBJ-1.Inline { display:inline-block;position:relative;line-height:normal; }
+      .OBJ-1 span,.OBJ-1:link span,.OBJ-1:visited span { color:#ffffff;font-family:Arial,sans-serif;font-weight:bold;text-decoration:none;text-align:center;text-transform:none;font-style:normal;left:6px;top:16px;width:175px;height:25px;font-size:19px;display:block;position:absolute;cursor:pointer; }
+      .OBJ-1.Disabled span,a:link.OBJ-1.Disabled span,a:visited.OBJ-1.Disabled span,a:hover.OBJ-1.Disabled span,a:active.OBJ-1.Disabled span { top:15px; }
+      .P-2 { text-align:center;line-height:1px;font-family:"Verdana", sans-serif;font-style:normal;font-weight:normal;color:#000000;background-color:transparent;font-variant:normal;font-size:16.0px;vertical-align:0; }
+      .C-3 { line-height:18.00px;font-family:"Verdana", sans-serif;font-style:normal;font-weight:normal;color:#000000;background-color:transparent;text-decoration:none;font-variant:normal;font-size:16.0px;vertical-align:0; }
+      .C-4 { line-height:23.00px;font-family:"Arial Black", sans-serif;font-style:normal;font-weight:700;color:#000000;background-color:transparent;text-decoration:none;font-variant:normal;font-size:16.0px;vertical-align:0; }
+    </style>
+    <script type="text/javascript" src="<?= $landing_resources_url.'wpscripts/jquery.js'?>"></script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+      $("a.ActiveButton").bind({ mousedown:function(){if ( $(this).attr('disabled') === undefined ) $(this).addClass('Activated');}, mouseleave:function(){ if ( $(this).attr('disabled') === undefined ) $(this).removeClass('Activated');}, mouseup:function(){ if ( $(this).attr('disabled') === undefined ) $(this).removeClass('Activated');}});
+      });
+    </script>
+  </head>
+  <body style="height:1000px;background:#ffffff;">
+    <div id="divMain" style="background:transparent;margin-left:auto;margin-right:auto;position:relative;width:960px;height:1000px;">
+      <div style="position:absolute;left:24px;top:468px;width:911px;height:451px;overflow:hidden;">
+        <p class="Body P-1"><span class="C-1">El Piso Pélvico es en conjunto de músculos que rodean la vagina y su debilidad es causa de Incontinencia Urinaria y disfunciones sexuales.</span></p>
+        <p class="Body P-1"><span class="C-1">El <span class="C-2">entrenador Vaginal </span>ayuda a tonificar los músculos del piso pélvico ya que está diseñado para rehabilitar músculos débiles, &nbsp;dando la capacidad de controlarlos conscientemente, solucionando de forma eficaz la Incontinencia Urinaria, ayudando a la rehabilitación después del embarazo y aumentando el placer sexual, ya que un músculo tonificado aumenta la fuerza de las contracciones orgásmicas y por lo tanto, la posibilidad de orgasmos más seguidos y potentes.</span></p>
+      </div>
+      <a href="http://sanaquefarma.com/product/show_product_by_id/43289" target="_blank" id="btn_1" class="OBJ-1 ActiveButton" style="position:absolute;left:386px;top:909px;width:188px;height:56px;">
+        <span>¡&nbsp;Compre&nbsp;Ahora&nbsp;!</span>
+      </a>
+      <div style="position:absolute;left:291px;top:967px;width:379px;height:26px;overflow:hidden;">
+        <p class="Body P-2"><span class="C-3">Línea para Pedidos </span><span class="C-4">300 3120 </span><span class="C-3">Bogotá D.C.</span></p>
+      </div>
+      <img alt="ENTRENADOR VAGINAL" src="<?= $landing_resources_url.'wpimages/wp92b8eb31_06.png'?>" style="position:absolute;left:216px;top:34px;width:529px;height:48px;">
+      <img alt="" src="<?= $landing_resources_url.'wpimages/wp00d5ef95_06.png'?>" style="position:absolute;left:332px;top:104px;width:295px;height:337px;">
+      <img alt="" src="<?= $landing_resources_url.'wpimages/wpe4b93671_06.png'?>" style="position:absolute;left:207px;top:559px;width:546px;height:398px;">
+    </div>
+  </body>
+</html>
